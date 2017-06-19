da4f5a658895a6ffdb376721cdc1fddf082f30a0
Ernie Yang <ernieyang09@hotmail.com>
2017-06-19 19:06:32 +0800

    修改首頁

--- App/features/intro/content.jsx
+++ App/features/intro/content.jsx
@@ -20,28 +20,50 @@ class Intro extends React.Component {
     function resizeCanvas() {
       canvas.setHeight(window.innerHeight * 0.85);
       canvas.setWidth(window.innerWidth);
-      canvas.renderAll();
     }
 
     window.addEventListener('resize', resizeCanvas, false);
 
     const getRandomInt = fabric.util.getRandomInt;
-    const rainbow =['#ffcc66', '#ccff66', '#66ccff', '#ff6fcf', '#ff6666'];
+    const rainbow = ['#ffcc66', '#ccff66', '#66ccff', '#ff6fcf', '#ff6666'];
     const rainbowEnd = rainbow.length - 1;
     const dotArr = [];
 
-    for (let i = 0; i < 100; i += 1) {
+    for (let i = 0; i < 900; i += 1) {
+      const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+      const top = canvas.height * 0.5 + plusOrMinus * canvas.height * 0.5 * Math.random();
+      const degree = Math.asin(Math.abs(canvas.height * 0.5 - top)/(canvas.width * 0.4));
+
       const dot = new fabric.Circle({
-        left: getRandomInt(0, canvas.width),
-        top: getRandomInt(0, canvas.height),
-        radius: getRandomInt(2, 8),
-        fill: rainbow[getRandomInt(0, rainbowEnd)]
+        left: (canvas.width *0.2 + canvas.width * 0.4 * Math.cos(degree)) * Math.random(),
+        top: top,
+        radius: getRandomInt(3, 9),
+        fill: rainbow[getRandomInt(0, rainbowEnd)],
       });
       dot.vx = 0;
       dot.vy = 0;
       canvas.add(dot);
       dotArr.push(dot);
     }
+
+      const  msg1 = new fabric.Text('WelCome To my', {
+              fontFamily: 'PingFang TC',
+              fontSize: fabric.util.parseUnit('2em'),
+              fill: 'white',
+              fontWeight: 'bold',
+              left: canvas.width - fabric.util.parseUnit('24em'),
+              top: canvas.height * 0.5 - fabric.util.parseUnit('1em'),
+            });
+      const  msg2 = new fabric.Text('Webpage', {
+              fontFamily: 'PingFang TC',
+              fontSize: fabric.util.parseUnit('2em'),
+              fill: 'white',
+              fontWeight: 'bold',
+              left: canvas.width - fabric.util.parseUnit('17em'),
+              top: canvas.height * 0.5 + fabric.util.parseUnit('1em'),
+            });
+
+        canvas.add(msg1).add(msg2);
 
     function animate() {
       for (let dot of dotArr) {
@@ -63,8 +85,11 @@ class Intro extends React.Component {
         vy += Math.random() - 0.5;
         dot.left += vx;
         dot.top += vy;
+        const degree = Math.asin(Math.abs(canvas.height * 0.5 - dot.top)/(canvas.width * 0.4));
+        const maxWidth = canvas.width *0.2 + canvas.width * 0.4 * Math.cos(degree);
 
-        if (dot.left < 0 || dot.left > canvas.width || dot.top < 30 || dot.top > canvas.height) {
+
+        if (dot.left < 0 || dot.left > maxWidth || dot.top < 0 || dot.top > canvas.height) {
           var r = Math.atan2(dot.top - canvas.height / 2, dot.left - canvas.width / 2);
           vx = -Math.cos(r);
           vy = -Math.sin(r);
@@ -77,10 +102,8 @@ class Intro extends React.Component {
       fabric.util.requestAnimFrame(animate, canvas.getElement());
       canvas.renderAll();
     }
-    animate();
-    // animate();
-    // animate();
-
+     animate();
+    canvas.renderAll();
   }
 
   render () {
